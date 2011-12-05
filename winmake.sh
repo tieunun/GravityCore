c6ec1433793922784d4eab66d7b0e97fdd95857f
Taylor <richbergert@gmail.com>
2011-12-05 13:48:20 -0700

    updated winmake.sh
    Removed -v flag from the compiler options in winmake.sh; it was
    unnecessary and annoying.

--- winmake.sh
+++ winmake.sh
@@ -10,7 +10,7 @@ fi
 
 mkdir -vp ./win
 
-i486-mingw32-g++ -v ./*xx -o ./win/Game.exe -mwindows -lmingw32 -lsfml-system -lsfml-window -lsfml-graphics -lsfml-audio -lBox2D -I/home/taylor/.wine/drive_c/SFML/include -I/home/taylor/.wine/drive_c/Box2D -L/home/taylor/.wine/drive_c/SFML/lib -L/home/taylor/.wine/drive_c/Box2D/Box2D -static-libstdc++ -static-libgcc
+i486-mingw32-g++ ./*xx -o ./win/Game.exe -mwindows -lmingw32 -lsfml-system -lsfml-window -lsfml-graphics -lsfml-audio -lBox2D -I/home/taylor/.wine/drive_c/SFML/include -I/home/taylor/.wine/drive_c/Box2D -L/home/taylor/.wine/drive_c/SFML/lib -L/home/taylor/.wine/drive_c/Box2D/Box2D -static-libstdc++ -static-libgcc
 
 cp -v /home/taylor/.wine/drive_c/windows/system32/OpenAL32.dll ./win
 cp -v /home/taylor/.wine/drive_c/SFML/lib/sfml-{audio,graphics,system,window}-2.dll ./win
